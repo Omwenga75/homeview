0e0ecc89ccaf43cd22565a90b60dd71b963c2b6e
Omwenga75 <Omwenga75@users.noreply.github.com>
2026-07-05 15:46:19 +0300

    Add password toggle icon and fix API_BASE for Vercel

--- static/main.js
+++ static/main.js
@@ -2,7 +2,7 @@
 
 const HV_CONFIG = {
     USE_MOCK: false,
-    API_BASE: 'http://127.0.0.1:8000', // Pointing to backend
+    API_BASE: window.location.hostname === '127.0.0.1' || window.location.hostname === 'localhost' ? 'http://127.0.0.1:8000' : '', // Pointing to backend
 };
 
 // --- Managers ---
